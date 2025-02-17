15fc06f58361dfb0181775b2fe630ddd90337278
Fabien Fournier <140694478+uncodestudio@users.noreply.github.com>
2025-02-17 15:26:34 +0400

    Updating map-animation

---
+++ map-animation.js
@@ -0,0 +1,31 @@
+//map animation
+$(".region").hover(function () {
+  let regionSlug = $(this).data("region");
+  let popup = $(".region_popup[data-region='" + regionSlug + "']");
+
+  if (popup.length > 0) {
+    popup.css("display", "block");
+
+    // Animation de slide-in
+    gsap.fromTo(popup, { opacity: 0, y: 20 }, {
+      opacity: 1,
+      y: 0,
+      duration: 0.3,
+      ease: "power2.out"
+    });
+  }
+}, function () {
+  let regionSlug = $(this).data("region");
+  let popup = $(".region_popup[data-region='" + regionSlug + "']");
+
+  // Animation de slide-out
+  gsap.to(popup, {
+    opacity: 0,
+    y: 20,
+    duration: 0.3,
+    ease: "power2.in",
+    onComplete: function () {
+      popup.css("display", "none");
+    }
+  });
+});
